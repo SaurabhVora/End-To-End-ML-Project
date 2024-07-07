71f2d5a743ada2859567f3d56208208fecccddfa
Saurabh Vora <saurabhvora27@gmail.com>
2024-07-07 22:29:31 +0530

    Make prediction

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-
+import pickle
 from sklearn.metrics import r2_score
 import dill
 
@@ -40,3 +40,11 @@ def evaluate_model(X_train, y_train,X_test,y_test, models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
